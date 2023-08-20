dff591dc5c91dc748c4be92ae0e995c181c1be04
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-08-20 15:01:02 +0700

    add detail page

---
+++ project1/src/views/event/AdvisorDetails.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { type AdvisorItem } from '@/type';
+import type { PropType } from 'vue';
+defineProps({
+    advisor: {
+        type: Object as PropType<AdvisorItem>,
+            require: true
+    }
+})
+</script>
+
+<template>
+    <div v-if="advisor">
+        <img :src="advisor.image">        
+        <p>ID: {{ advisor.id }}</p>
+        <p>Name-Surname: {{ advisor.name }} {{ advisor.surname }}</p>
+    </div>
+</template>
---
+++ project1/src/views/event/StudentDetails.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { type StudentItem } from '@/type';
+import type { PropType } from 'vue';
+defineProps({
+    student: {
+        type: Object as PropType<StudentItem>,
+            require: true
+    }
+})
+</script>
+
+<template>
+    <div v-if="student">
+        <img :src="student.image">        
+        <p>ID: {{ student.id }}</p>
+        <p>Name-Surname: {{ student.name }} {{ student.surname }}</p>
+        <p>Course: {{ student.course }}</p>
+    </div>
+</template>
