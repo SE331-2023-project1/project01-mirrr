a7f21f08e52ecd729e12f53f71ee8b630760ab5e
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-08-23 21:51:47 +0700

    fix some tailwind in Student Page

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router'
           />
         </div>        
     </nav>
-    <div class="space-x-7 text-center font-mono">
+    <div class="space-x-7 text-center font-mono text-xl pt-2 pb-5">
           <RouterLink class="text-black" to="/">Student</RouterLink>
           <RouterLink class="text-black" to="/advisor">Advisor</RouterLink>
     </div>
--- project1/src/components/StudentCard.vue
+++ project1/src/components/StudentCard.vue
@@ -11,41 +11,8 @@ const props = defineProps({
 
 
 <template>
-    <div class="border-2 border-indigo-600 h-50 w-60 flex flex-col items-center hover:shadow-lg hover:scale-125">
+    <div class="border-2 border-indigo-600 h-50 w-52 flex flex-col items-center hover:shadow-lg hover:scale-110">
         <img :src="student.image" alt="image" class="object-scale-down h-36 w-57 mt-4">
         <h4>{{ student.name}} {{ student.surname }}</h4>
     </div>
 </template>
-
-<!-- <style scoped>
-.student-card {
-    padding: 20px;
-    width: 250px;
-    cursor: pointer;
-    border: 5px solid #ffffff;
-    margin-bottom: 18px;
-    margin-top: 18px;
-    border-radius: 15px;
-    margin-right: 60px;
-}
-
-
-
-.student-card:hover {
-    transform: scale(1.01);
-    box-shadow: 0 3px 12px 0 rgba(86, 18, 18, 0.2);
-}
-
-.student-link {
-    color: #2c3e50;
-    text-decoration: none;
-}
-
-.image {
-    width: 200px;
-    height: 150px;
-    align-items: center;
-    margin-top: 10%;
-    border-radius: 10px
-}
-</style> -->
