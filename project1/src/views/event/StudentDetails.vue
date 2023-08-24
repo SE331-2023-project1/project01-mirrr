e48f91c160df2c9346b11f4122b8624737f144cd
Areeya <amyschclass@gmail.com>
2023-08-24 20:36:36 +0700

    Edit student detail

--- project1/src/views/event/StudentDetails.vue
+++ project1/src/views/event/StudentDetails.vue
@@ -36,14 +36,23 @@ StudentService.getStudentById(Number(props.id))
 }).catch(error => {
     console.log(error)
 })
+
 </script>
 
 <!--Template-->
 <template>
     <div v-if = "student">
-        <img :src="student.image">        
-        <p>ID: {{ student.id }}</p>
-        <p>Name-Surname: {{ student.name }} {{ student.surname }}</p>
-        <p>Course: {{ student.course }}</p>
+        <div class="m-5">
+            <div class="border-2 bg-gradient-to-r from-[#075985] to-[#082f49] h-50 w-full flex md:flex-row gap-2 flex-col p-3 rounded-md">
+                <div class="border-2 border-[#0c4a6e] h-55 w-80 flex flex-col items-center rounded-md">
+                    <img :src="student.image" class="object-scale-down rounded-md">
+                </div>
+                <div class="border-2  border-[#94a3b8] bg-[#94a3b8] h-50 w-full flex flex-col items-left rounded-md">        
+                    <p>ID: {{ student.id }}</p>
+                    <p>Name-Surname: {{ student.name }} {{ student.surname }}</p>
+                    <p>Course: {{ student.course }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
