92713503679b5f67aca273f2b9746ff9e8132858
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-08-24 23:17:46 +0700

    fix advisor

--- project1/src/views/event/AdvisorDetails.vue
+++ project1/src/views/event/AdvisorDetails.vue
@@ -42,17 +42,17 @@ AdvisorService.getAdvisorById(Number(props.id))
 <template>
     <div v-if="advisor">
         <div class="m-5">
-            <div class="font-mono text-2xl mx-auto border-2 bg-gradient-to-r from-[#075985] to-[#082f49] h-50 w-1/2 flex md:flex-row gap-2 flex-col p-3 rounded-md">
-                <div class="border-2 border-[#0c4a6e] h-55 w-80 flex flex-col items-center rounded-md">
+            <div class="font-mono text-3xl mx-auto border-2 bg-gradient-to-r bg-green-700 h-50 w-1/2 flex md:flex-row gap-2 flex-col p-3 rounded-md">
+                <div class="border-green-700 border-2 h-55 w-80 flex flex-col items-center rounded-md">
                     <img :src="advisor.image" class="object-scale-down rounded-md">
                 </div>
-                <div class="border-2  border-[#94a3b8] bg-[#94a3b8] h-50 w-full flex flex-col items-left rounded-md">        
+                <div class="pl-2 border-2 bg-white h-50 w-full flex flex-col items-left rounded-md">        
                     <p>ID: {{ advisor.id }}</p>
                     <p>Name-Surname: {{ advisor.name }} {{ advisor.surname }}</p>
                     <p></p>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </template>
