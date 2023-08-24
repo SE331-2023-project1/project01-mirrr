94bbc95a14d6045a3be28b688c9c6992f6b0159a
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-08-24 21:16:16 +0700

    Fix Comment

--- project1/src/views/CommentView.vue
+++ project1/src/views/CommentView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="text-center font-mono">
-    <p class="mt-5 text-5xl">Comment</p>
+  <div class="mt-3 mx-auto font-mono text-center border-blue-600 border-2 w-1/4">
+    <p class="text-center mt-5 text-5xl">Comment</p>
+    <br>
     <p class="text-3xl">Student:</p>
-    <select v-model="selectedStudent" class="border-black border-2 w-64 h-10 text-xl">
+    <select v-model="selectedStudent" class="border-black border-2 w-96 h-10 text-xl">
       <option value="" disabled>Select Student</option>
       <option v-for="student in students" :key="student.id" :value="student.id">{{ student.name }} {{ student.surname }}</option>
     </select>
+    <br>    
+    <br>
     <p class="text-3xl">Comment:</p>
-    <input v-model="comment" class="border-black border-2 w-64 h-20"/>
-    <button class="text-xl">Send</button>
+    <input v-model="comment" class="border-black border-2 p-2.5 h-20 mb-5 w-96 text-xl"/>
+    <br>
+    <button @click="submitComment" class="mt-1 mb-3 px-4 py-2 bg-blue-500 text-white rounded">Submit</button>
   </div>
 </template>
 
-<script setup>
+<!-- textarea {
+  width: 95%;
+  height: 70px;
+  padding: 10px;
+  font-size: 20px;
+  margin-bottom: 20px;
+} -->
+
+<script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
@@ -28,6 +40,15 @@ onMounted(async () => {
     console.error('Error fetching data:', error);
   }
 });
+
+const submitComment = () => {
+  // Do something with the selectedStudent and comment values, like sending to a server
+  console.log('Selected Student:', selectedStudent.value);
+  console.log('Comment:', comment.value);
+  // Reset the form
+  selectedStudent.value = '';
+  comment.value = '';
+};
 </script>
 
 <style>
