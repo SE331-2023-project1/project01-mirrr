e2fb28999845572353f1b8e17cc273ce3393a083
beauti21kk <kornkanok_kanchana@cmu.ac.th>
2023-08-24 20:19:09 +0700

    fix comment

--- project1/src/views/CommentView.vue
+++ project1/src/views/CommentView.vue
@@ -1,19 +1,35 @@
-<script setup lang="ts">
-import StudentCard from '@/components/StudentCard.vue';
-import type { StudentItem } from '@/type';
-import { ref } from 'vue'
-import type { Ref } from 'vue'
-import axios from 'axios'
-import StudentService from '@/services/StudentService'
+<template>
+  <div class="text-center font-mono">
+    <p class="mt-5 text-5xl">Comment</p>
+    <p class="text-3xl">Student:</p>
+    <select v-model="selectedStudent" class="border-black border-2 w-64 h-10 text-xl">
+      <option value="" disabled>Select Student</option>
+      <option v-for="student in students" :key="student.id" :value="student.id">{{ student.name }} {{ student.surname }}</option>
+    </select>
+    <p class="text-3xl">Comment:</p>
+    <input v-model="comment" class="border-black border-2 w-64 h-20"/>
+    <button class="text-xl">Send</button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const selectedStudent = ref('');
+const students = ref([]);
+const comment = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('db.json');
+    students.value = response.data.student;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
 </script>
 
-<template>
-    <p class="mt-5 text-center font-mono text-5xl">Comment</p>
-<div class="border-blue-500 border-2 h-50 w-52 flex items-center">
-    <p>Student:</p>
-    
-    <p>Comment:</p>
-        <input class="border-blue-500">
-
-</div>
-</template>
+<style>
+/* Add your styles here */
+</style>
